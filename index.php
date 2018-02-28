2469dd41969e47d4113e2ac55ae502db7c6f28b1
Parthik Ghosh <31651344+ParthikGhosh@users.noreply.github.com>
2018-02-28 19:51:28 +0530

    Create index.php
    initial commit.

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$dbhost = getenv("uri");
+$dbuser = getenv("username");
+$dbpwd = getenv("password");
+$dbname = getenv("database_name");
+$connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+if ($connection->connect_errno) {
+    printf("<br><h2>Mysql backing service secrets missing...</h2><br>");
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+echo "<br><h2>Mysql backing services provisioned via service broker is available...</h2><br>";
+$connection->close();
+?>
